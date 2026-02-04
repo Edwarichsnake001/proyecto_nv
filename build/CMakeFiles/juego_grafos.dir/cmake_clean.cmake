d08eeaa888d71cec2cb368c6dbc9b2656bb4ce5e
Edwarichsnake001 <materoge32@gmail.com>
2026-02-04 10:05:29 -0500

    Ajustes

---
+++ build/CMakeFiles/juego_grafos.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/juego_grafos.dir/main.cpp.obj"
+  "CMakeFiles/juego_grafos.dir/main.cpp.obj.d"
+  "CMakeFiles/juego_grafos.dir/src/Modelo.cpp.obj"
+  "CMakeFiles/juego_grafos.dir/src/Modelo.cpp.obj.d"
+  "juego_grafos.exe"
+  "juego_grafos.exe.manifest"
+  "juego_grafos.pdb"
+  "libjuego_grafos.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/juego_grafos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
